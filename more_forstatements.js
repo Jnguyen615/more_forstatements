3736b29d49c321b031580158c90156a7a732e7f6
Jnguyen615 <Jenniferannnguyen@live.com>
2023-02-06 11:02:38 -0700

    inital commit

---
+++ more_forstatements.js
@@ -0,0 +1,47 @@
+// // var pets = ["Cat", "Dog", "Fish", "Hamster", "Hedgehog", "Gecko", "Mice"];
+// // for (var element of pets){
+// //     if (element.length === 4)
+// //     console.log(element);
+// // }
+
+// // var words = ["ToDay WaS a GrEaT DaY!"];
+// // for (i=0; i < words.length; i++) {
+// //     words[i] = words[i].toLowerCase();
+// // }
+// // console.log(words) 
+
+// // var countries = ["United States", "Vietnam", "China", "Japan", "Taiwan"];
+// // for (land of countries);
+// //     if(land.startsWith("T")); {
+// //         console.log(land)
+// //     }
+
+// // var activities = ["Skating", "Fishing", "Golf", "Swimming", "Croquet", ];
+// // for (act of activities)
+// //     if(act.endsWith("ing")){
+// //         console.log(act)
+// //     }
+
+// // var destinations = ["Vietnam", "Laos", "Jamaica", "Ukraine", "Argentina"];
+// //     destinations.sort()    
+// //     for (country of destinations) {   
+// //         // console.log("sorted destinations", destinations)
+                  
+// //         // console.log("sorted destinations", destinations)
+// //         console.log("countries", country);
+// //     }
+
+// // Put all word initial letters to uppercase   
+// var sentence = ["Today the weather was rather bad."];
+// function changeCase (str) {
+//     str = str.split(" ");
+//     for (var i = 0; i < str.length; i++) {
+//         str[i] = str[i][0].toUpperCase() + str[i].substr(1);
+//     } 
+//         return str.join(" ");
+    
+// }
+// console.log(changeCase("Today the weather was rather bad"));
+
+// /Partner practice 
+
